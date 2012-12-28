4c8de75b55d75f103d84290edf8be56cb7e28358
Steven Weaver <weaverst@gmail.com>
2012-12-28 13:44:58 -0800

    updated routes to work completely with mongoose

--- app/routes/sequence.js
+++ app/routes/sequence.js
@@ -1,3 +1,4 @@
+
 var mongoose = require('mongoose')
   , Sequence = mongoose.model('Sequence');
 
@@ -6,6 +7,7 @@ exports.findById = function(req, res) {
 
    var id = req.params.id;
    console.log('Retrieving sequence: ' + id);
+
    Sequence.findOne({_id : id}, function(err, items) {
       if (err)
          res.send('There is no sequence with id of ' + id);
@@ -17,63 +19,71 @@ exports.findById = function(req, res) {
 
 //return all sequences
 exports.findAll = function(req, res) {
-    db.collection('sequences', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
-    });
+
+   Sequence.find({},function(err, items) {
+      if (err)
+         res.send('There is no sequence with id of ' + id);
+       else
+         res.send(items);
+   });
+
 };
 
 //upload a sequence
 exports.addSequence = function(req, res) {
-    var sequence = req.query;
-    console.log('Adding sequence: ' + JSON.stringify(sequence));
-    db.collection('sequences', function(err, collection) {
-        collection.insert(sequence, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
+
+    postdata = req.query;
+    console.log('Adding sequence: ' + JSON.stringify(postdata));
+
+    //Should check the postdata before
+    Sequence.create(postdata, function (err, result) {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            console.log('Success: ' + JSON.stringify(result));
+            res.send(result);
+        }
     });
+
 }
 
 //update a sequence
 exports.updateSequence = function(req, res) {
+
     var id = req.params.id;
-    var sequence = req.body;
+    var postdata = req.body;
+
     console.log('Updating sequence: ' + id);
     console.log(JSON.stringify(sequence));
 
-    db.collection('sequences', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, sequence, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating sequence: ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.send(sequence);
-            }
-        });
+    //Should check the postdata before
+    Sequence.update(postdata, function (err, result) {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            console.log('Success: ' + JSON.stringify(result));
+            res.send(postdata);
+        }
     });
-
+ 
 }
 
 //delete a sequence
 exports.deleteSequence = function(req, res) {
+
     var id = req.params.id;
     console.log('Deleting sequence: ' + id);
-    db.collection('sequences', function(err, collection) {
-        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
+
+    Sequence.remove({ _id: new BSON.ObjectID(id) }, function(err) {
+        if (err) {
+            res.send({'error':'An error has occurred - ' + err});
+        }
+
+        else {
+            console.log('' + result + ' document(s) deleted');
+            res.send(req.body);
+         }
     });
+    
 }
 
--- server.js
+++ server.js
@@ -44,4 +44,3 @@ app.listen(3000);
 console.log('Listening on port 3000...');
 module.exports = app;
 
-
