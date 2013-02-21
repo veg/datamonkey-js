c15c1a5d8c87b296160d74933f0e1964551521e5
Steven Weaver <weaverst@gmail.com>
2013-02-20 17:22:04 -0800

    Fixed post error. Job querying correctly. Event triggers when job complete.
    Next:
    Parse results.
    Integrate socket.io
    Refactor analysis models if need be
    Create each of the analysis. (Finally)
    
    I expect completion of this by end of month February 2013.

--- app/routes/meme.js
+++ app/routes/meme.js
@@ -22,36 +22,39 @@ exports.findAll = function(req, res) {
 
 };
 
-
 //Start a Meme Analysis
 exports.addMeme = function(req, res) {
 
   postdata = req.query;
- 
   seqid =  postdata.seqid;
-  delete postdata.seqid;
 
+  //Create Meme Analysis
   var meme = new Meme({
     msafn  : seqid,
     status : globals.queue,
   });
   
-  //Save the Parent Meme
   meme.save(function (err,result) {
 
     if (err) return handleError(err);
 
+    //Create Meme Parameters
     var parameters = new MemeParameters({
+
       modelstring : postdata.modelstring,
       treemode    : postdata.treemode,
       pvalue      : postdata.pvalue,
+
     });
     
+    //Save Meme Parameters to parent Meme object
     parameters.save(function (err, meme_result) {
       if (err) return handleError(err);
 
       else {
-        //Open Multiple Sequence Alignment File
+
+        // Open Multiple Sequence Alignment File to get all necessary parameters
+        // for dispatching.
         SequenceAlignmentFile.findOne({_id : seqid}, function(err, msa) {
           if (err)
             res.send('There is no sequence with id of ' + id);
@@ -59,24 +62,24 @@ exports.addMeme = function(req, res) {
            else {
 
              //Take the current parameters, and add MEME specific params
-             //MEME specific params should be in globals
+             //TODO: MEME specific params should be in globals
              var params = {
                 'method'        : globals.MEME,
                 'treeMode'      : meme_result.treemode,
                 'root'          : "SRC1",
-                'modelString'   : meme_result.modelstring,
-                'NamedModels'   : "",
-                'rOptions'      : 4,
-                'dNdS'          : 1.0,
-                'ambChoice'     : 0,
-                'pValue'        : meme_result.pvalue,
-                'rateOption'    : 0,
-                'rateClasses'   : 2,
-                'rateOption2'   : 1,
-                'rateClasses2'  : 2
+                'modelstring'   : meme_result.modelstring,
+                'namedmodels'   : "",
+                'roptions'      : 4,
+                'dnds'          : 1.0,
+                'ambchoice'     : 0,
+                'pvalue'        : meme_result.pvalue,
+                'rateoption'    : 0,
+                'rateclasses'   : 2,
+                'rateoption2'   : 1,
+                'rateclasses2'  : 2
              };
 
-             //Dispatch the analysis to the perl scripts
+             //Dispatch the analysis to the perl script
              dpl.dispatchAnalysis(meme._id,msa,params,res);
 
              //TODO: We should return the status id ticket
@@ -103,7 +106,7 @@ exports.queryStatus = function(req, res) {
    else {
       //This should eventually be its own polling task
       //That pushes out to the user
-      var hello = dme.jobListener.start(globals.types.meme, item);
+      dme.jobListener.start(globals.types.meme, item);
    }
 
   });
