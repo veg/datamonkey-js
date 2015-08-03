2317d5bcbc54f9f9690c053dc78d1ed22e2634ef
Steven Weaver <sweaver@ucsd.edu>
2015-08-03 16:57:16 -0700

    adding betweenness centrality algorithm

--- public/assets/lib/datamonkey/hivtrace/misc.js
+++ public/assets/lib/datamonkey/hivtrace/misc.js
@@ -1,133 +1,239 @@
-function compute_shortest_paths(cluster, edges) {
+function hivtrace_cluster_adjacency_list(obj) {
 
-  // Floyd-Warshall implementation
-  var distances = {}
+  var nodes = obj.Nodes,
+      edges = obj.Edges;
 
-  var nodes =  cluster.nodes;
-  node_ids = []
-  nodes.forEach(function(n) { node_ids.push(n.id)});
 
-  // Step 0: Filter edges that only exist within the cluster
-  var new_edges = edges.filter(function(n) { 
-    if( node_ids.indexOf(n.sequences[0]) != -1 && node_ids.indexOf(n.sequences[1]) != -1) {
-      return true;
-    } else {
-      return false;
+  var adjacency_list = {};
+
+  edges.forEach(function(e, i) {
+
+    function in_nodes(n, id) {
+      return n.id == id;
     }
-   });
 
-  // Step 1: Initialize distances
-  nodes.forEach(function(n) { distances[n.id] = {} });
-  nodes.forEach(function(n) { distances[n.id][n.id] = 0 });
+    var seq_ids = e["sequences"];
 
-  // Step 2: Initialize distances with edge weights
-  new_edges.forEach(function(e){
-    distances[e.sequences[0]] = {};
-    distances[e.sequences[1]] = {};
+    var n1 = nodes.filter(function(n) { return in_nodes(n, seq_ids[0]) })[0],
+        n2 = nodes.filter(function(n) { return in_nodes(n, seq_ids[1]) })[0];
+
+    adjacency_list[n1.id] ? adjacency_list[n1.id].push(n2) : adjacency_list[n1.id] = [n2];
+    adjacency_list[n2.id] ? adjacency_list[n2.id].push(n1) : adjacency_list[n2.id] = [n1];
+
   });
 
-  new_edges.forEach(function(e){
-    distances[e.sequences[0]][e.sequences[1]] = 1;
-    distances[e.sequences[1]][e.sequences[0]] = 1;
-  });
 
-  // Step 3: Get shortest paths
-  nodes.forEach(function(k) {
-    nodes.forEach(function(i) {
-      nodes.forEach(function(j) {
-        if (i.id != j.id) {
-          var d_ik = distances[k.id][i.id];
-          var d_jk = distances[k.id][j.id];
-          var d_ij = distances[i.id][j.id];
-          if (d_ik != null &&  d_jk != null) {
-            d_ik += d_jk;
-            if ( d_ij == null || d_ij > d_ik ) {
-              distances[i.id][j.id] = d_ik;
-              distances[j.id][i.id] = d_ik;
+  return adjacency_list;
+
+}
+
+// Reconstructs path from floyd-warshall algorithm
+function hivtrace_get_path(next, i, j) {
+
+  var all_paths = [];
+  var i = parseInt(i);
+  var j = parseInt(j);
+
+  for (var c = 0; c < next[i][j].length; c++) {
+
+    var k = next[i][j][c];
+    var intermediate = k;
+
+    if(intermediate == null || intermediate == i) {
+      return [[parseInt(i), parseInt(j)]];
+    } else {
+
+      var paths_i_k = hivtrace_get_path(next, i, intermediate);
+      var paths_k_j = hivtrace_get_path(next, intermediate, j);
+
+      for (var i_k_index=0; i_k_index < paths_i_k.length; i_k_index++) {
+        var i_k = paths_i_k[i_k_index];
+        for (var k_j_index=0; k_j_index < paths_k_j.length; k_j_index++) {
+          var k_j = paths_k_j[k_j_index];
+          if(i_k.length) {
+            if((i_k[0] == i) && (i_k[i_k.length - 1] == k) && (k_j[0] == k) && (k_j[k_j.length - 1] == j)) {
+              i_k.pop()
+              all_paths.push(i_k.concat(k_j));
             }
           }
         }
-      });
-    });
-  });
+      }
+    }
+  }
 
-  return distances;
-
-}
-
-function hivtrace_compute_mean_path(cluster_id, nodes, edges, cluster_sizes) {
-
-  // Create a cluster object that is easy to deal with
-  var unique_clusters = d3.set(nodes.map(function(d) { return d.cluster })).values();
-  var cluster_map = {};
-  unique_clusters.map(function(d){ cluster_map[d] = {'size': 0, 'nodes': [] } });
-
-  //Add each node to the cluster
-  Object.keys(cluster_map).map(function(d){
-    cluster_map[d]['size'] = cluster_sizes[parseInt(d)-1];
-  });
-
-  nodes.map(function(d) { cluster_map[d.cluster]['nodes'].push(d) });
-
-  // Compute the shortest paths according to Floyd-Warshall algorithm
-  var distances = compute_shortest_paths(cluster_map[cluster_id], edges);
-
-  var path_sum = 0;
-
-  //Sum all the distances and divide by the number of nodes
-  Object.keys(distances).forEach( function(n) { 
-    var node = nodes.filter(function(a_node){return a_node.id==n})
-    Object.keys(distances[n]).forEach( function(k) {
-      path_sum += distances[n][k];
-    });
-  });
-
-  // Average Mean Path Calculation
-  // l = (sum(distances))/(N(N-1))
-  var node_len = cluster_map[cluster_id].nodes.length
-  var mean_path_length = path_sum / (node_len * (node_len - 1));
-  return mean_path_length;
+  return all_paths;
 
 }
 
-function hivtrace_compute_node_mean_paths(nodes, edges) {
+function hivtrace_paths_with_node(node, next, i, j) {
 
-  // Create a cluster object that is easy to deal with
-  var unique_clusters = d3.set(nodes.map(function(d) { return d.cluster })).values();
-  var cluster_map = {};
-  unique_clusters.map(function(d){ cluster_map[d] = {'size': 0, 'nodes': [] } });
+  var paths = hivtrace_get_path(next, i, j);
 
-  ////Add each node to the cluster
-  //Object.keys(cluster_map).map(function(d){
-  //  cluster_map[d]['size'] = cluster_sizes[parseInt(d)-1];
-  //});
+  // Retrieve intermediary paths
+  //console.log(paths);
+  paths = paths.map(function(sublist){ return sublist.slice(1, -1) });
 
-  nodes.map(function(d) { cluster_map[d.cluster]['nodes'].push(d) });
+  if(!paths) {
+    return 0;
+  }
 
-  Object.keys(cluster_map).forEach( function(cluster_map_index) {
-    // Compute the shortst paths according to Floyd-Warshall algorithm for each cluster
-    var distances = compute_shortest_paths(cluster_map[cluster_map_index], edges);
+  var num_nodes = [];
 
-    //Sum all the distances and divide by the number of nodes
-    Object.keys(distances).forEach( function(n) { 
-      var index=-1;
-      for(var x=0;x<nodes.length;x++) {
-        if(nodes[x].id===n) {
-          index=x;
-          break;
+  for (var i = 0; i < paths.length; i++) {
+    sublist = paths[i];
+    num_nodes.push(d3.sum(sublist.map(function(n) { return n == node; })));
+  }  
+
+  var mean = d3.mean(num_nodes);
+
+  if (mean == undefined) {
+    mean = 0;
+  }
+
+  //console.log(mean);
+
+  return mean;
+
+}
+
+
+// Same as compute shortest paths, but with an additional next parameter for reconstruction
+function hivtrace_compute_shortest_paths_with_reconstruction(obj, subset, use_actual_distances) {
+
+  // Floyd-Warshall implementation
+  var distances = [];
+  var next = [];
+  var nodes =  obj.Nodes;
+  var edges =  obj.Edges;
+  var node_ids = [];
+
+  //nodes.forEach( function(n) { return node_ids.push(n.id); } );
+  var adjacency_list = datamonkey.hivtrace.cluster_adjacency_list(obj);
+
+  if(!subset) {
+    subset = Object.keys(adjacency_list);
+  }
+
+  var node_count = subset.length;
+
+  for(var i=0; i<subset.length; i++) {
+    var a_node = subset[i];
+    var empty_arr = _.range(node_count).map(function(d) { return null });
+    var zeroes = _.range(node_count).map(function(d) { return null });
+    distances.push(zeroes);
+    next.push(empty_arr);
+  };
+
+  for(var index=0; index<subset.length; index++) {
+    var a_node=subset[index];
+    for(var index2=0; index2<subset.length; index2++) {
+      var second_node = subset[index2];
+      if (second_node != a_node) {
+        if (adjacency_list[a_node].map(function(n) { return n.id}).indexOf(second_node) != -1) {
+          distances[index][index2] = 1;
+          distances[index2][index] = 1;
         }
       }
-      var node = nodes[index];
-      var path_sum = 0;
-      var adj_nodes = Object.keys(distances[n]).length;
-      Object.keys(distances[n]).forEach( function(k) {
-        path_sum += distances[n][k];
-      });
-      node.mean_path_length = path_sum / adj_nodes;
-    });
-  });
+    }
+  }
+
+  for(var index_i=0; index_i<subset.length; index_i++) {
+    var n_i = subset[index_i];
+    for(var index_j=0; index_j<subset.length; index_j++) {
+      var n_j = subset[index_j];
+      if (index_i == index_j) {
+        next[index_i][index_j] = [];
+      } else {
+        next[index_i][index_j] = [index_i];
+      }
+    }
+  }
+
+  // clone distances
+  var distances2 = _.map(distances, _.clone); 
+  var c = 0;
+
+  for(var index_k=0;index_k<subset.length;index_k++) {
+    var n_k=subset[index_k];
+    for(var index_i=0;index_i<subset.length;index_i++) {
+      var n_i=subset[index_i];
+      for(var index_j=0;index_j<subset.length;index_j++) {
+        var n_j=subset[index_j];
+
+        if (n_i != n_j) {
+
+          d_ik = distances[index_k][index_i];
+          d_jk = distances[index_k][index_j];
+          d_ij = distances[index_i][index_j];
+
+          if (d_ik != null && d_jk != null) {
+            d_ik += d_jk;
+            if (d_ij == null || (d_ij > d_ik) ) {
+              distances2[index_i][index_j] = d_ik;
+              distances2[index_j][index_i] = d_ik;
+              next[index_i][index_j] = [];
+              next[index_i][index_j] = next[index_i][index_j].concat(next[index_k][index_j]);
+              continue;
+            } else if(d_ij == d_ik) {
+              next[index_i][index_j] = next[index_i][index_j].concat(next[index_k][index_j]);
+            }
+          }
+          c++;
+          distances2[index_j][index_i] = distances[index_j][index_i];
+          distances2[index_i][index_j] = distances[index_i][index_j];
+        }
+      }
+    }
+
+    var t = distances2; 
+    distances2 = distances;
+    distances = t;
+
+  }
+
+  return {'ordering': subset, 'distances': distances, 'next': next};
 
 }
+
+// Returns dictionary of nodes' betweenness centrality
+// Utilizes the Floyd-Warshall Algorithm with reconstruction
+function hivtrace_compute_betweenness_centrality(node, obj, paths, newsubset) {
+
+
+  if (!paths) {
+    paths = hivtrace_compute_shortest_paths_with_reconstruction(obj);
+  }
+
+  // find index of id
+  var index = paths['ordering'].indexOf(node);
+
+  if (index == -1) {
+    return null;
+  }
+
+
+  var length = paths['distances'].length;
+
+  if(length != 2) {
+    scale = 1 / ((length - 1) * (length - 2));
+  } else {
+    scale = 1;
+  }
+  
+
+  // If s->t goes through 1, add to sum
+  // Reconstruct each shortest path and check if node is in it
+  var paths_with_node = [];
+  for(i in _.range(length)) {
+    for(j in _.range(length)) {
+      paths_with_node.push(hivtrace_paths_with_node(index, paths['next'], i, j));
+    }
+  }
+  
+  return d3.sum(paths_with_node) * scale;
+
+}
+
 
 function hivtrace_compute_node_degrees(obj) {
 
@@ -183,7 +289,8 @@ if(typeof datamonkey.hivtrace == 'undefined') {
 }
 
 datamonkey.hivtrace.compute_node_degrees = hivtrace_compute_node_degrees;
-datamonkey.hivtrace.compute_mean_path = hivtrace_compute_mean_path;
-datamonkey.hivtrace.compute_node_mean_paths = hivtrace_compute_node_mean_paths;
+//datamonkey.hivtrace.compute_node_mean_paths = hivtrace_compute_node_mean_paths;
 datamonkey.hivtrace.export_csv_button = hivtrace_export_csv_button;
 datamonkey.hivtrace.convert_to_csv = hivtrace_convert_to_csv;
+datamonkey.hivtrace.betweenness_centrality = hivtrace_compute_betweenness_centrality;
+datamonkey.hivtrace.cluster_adjacency_list = hivtrace_cluster_adjacency_list;
--- tests/hivtrace/hivtrace_utils.js
+++ tests/hivtrace/hivtrace_utils.js
@@ -27,44 +27,158 @@
 
 */
 
-require('../../public/assets/lib/datamonkey/hivtrace/misc.js'),
-require('../../public/assets/lib/d3/d3.js');
-
 var fs     = require('fs'),
     path   = require('path'),
-    should = require('should');
+    should = require('should'),
+    _      = require('underscore'),
+    jsdom  = require("jsdom");
 
+var hiv_utils = fs.readFileSync(path.join(__dirname, '/../../public/assets/lib/datamonkey/hivtrace/misc.js'));
+var d3_js = fs.readFileSync(path.join(__dirname, '../../public/assets/lib/d3/d3.js'));
+var underscore = fs.readFileSync(path.join(__dirname, '../../public/assets/lib/underscore/underscore.js'));
 
 describe('convert object to csv', function() {
 
-  var trace_results = JSON.parse(fs.readFileSync(path.join(__dirname, './test.trace.json')));
-  var results = datamonkey.hivtrace.convert_to_csv(trace_results);
-  var parsed_obj = d3.csv.parse(results);
-
   it('elements should not be empty', function(done) {
 
-    // ensure each key exists and has a value
-    function hasNonEmptyElements(element, index, array) {
-      return (Boolean(element.seqid) && 
-              Boolean(element.cluster) && 
-              Boolean(element.degree) && 
-              Boolean(element.is_contaminant));
-    }
+    jsdom.env({
 
-    parsed_obj.every(hasNonEmptyElements).should.be.true;
-    done();
+      url: "http://datamonkey.org",
+      src: [d3_js, underscore, hiv_utils],
+      done: function (err, window) {
+
+          datamonkey = window.datamonkey;
+          d3 = window.d3;
+          trace_results = JSON.parse(fs.readFileSync(path.join(__dirname, './test.trace.json')));
+          results = datamonkey.hivtrace.convert_to_csv(trace_results);
+          parsed_obj = d3.csv.parse(results);
+
+          // ensure each key exists and has a value
+          function hasNonEmptyElements(element, index, array) {
+            return (Boolean(element.seqid) && 
+                    Boolean(element.cluster) && 
+                    Boolean(element.degree) && 
+                    Boolean(element.is_contaminant));
+          }
+
+          parsed_obj.every(hasNonEmptyElements).should.be.true;
+          done();
+
+      }
+     });
+
+
+
 
   });
 
   it('elements should mark contaminants correctly', function(done) {
 
-    // ensure that NL4-3 and HXB2_env are tagged as contaminants
-    var contaminants = parsed_obj.filter(function(elem){return elem.is_contaminant == 'true'});
-    var contaminant_ids = contaminants.map(function(elem) {return elem['seqid']})
-    contaminant_ids.indexOf('NL4-3').should.not.be.equal(-1);
-    contaminant_ids.indexOf('HXB2_env').should.not.be.equal(-1);
+    jsdom.env({
+
+      url: "http://datamonkey.org",
+      src: [d3_js, underscore, hiv_utils],
+      done: function (err, window) {
+
+        datamonkey = window.datamonkey;
+        d3 = window.d3;
+
+        trace_results = JSON.parse(fs.readFileSync(path.join(__dirname, './test.trace.json')));
+        results = datamonkey.hivtrace.convert_to_csv(trace_results);
+        parsed_obj = d3.csv.parse(results);
+
+        // ensure that NL4-3 and HXB2_env are tagged as contaminants
+
+        // Should mark which contaminant
+        var contaminants = parsed_obj.filter(function(elem){return elem.is_contaminant == 'true'});
+        var contaminant_ids = contaminants.map(function(elem) {return elem['seqid']});
+        contaminant_ids.indexOf('NL4-3').should.not.be.equal(-1);
+        contaminant_ids.indexOf('HXB2_env').should.not.be.equal(-1);
+        done();
+
+      }
+     });
+
+  });
+
+});
+
+describe('betweenness centrality', function() {
+
+
+  var trace_results = JSON.parse(fs.readFileSync(path.join(__dirname, './krackhardt_kite.json')));
+
+  it('should correctly compute', function(done) {
+
+    jsdom.env({
+      url: "http://datamonkey.org",
+      src: [d3_js, underscore, hiv_utils],
+      done: function (err, window) {
+
+          var datamonkey = window.datamonkey;
+          var delta = .001;
+          
+          var heather  = datamonkey.hivtrace.betweenness_centrality('Heather', trace_results),
+              fernando = datamonkey.hivtrace.betweenness_centrality('Fernando', trace_results),
+              garth    = datamonkey.hivtrace.betweenness_centrality('Garth', trace_results),
+              ike      = datamonkey.hivtrace.betweenness_centrality('Ike', trace_results),
+              diane    = datamonkey.hivtrace.betweenness_centrality('Diane', trace_results),
+              andre    = datamonkey.hivtrace.betweenness_centrality('Andre', trace_results),
+              beverly  = datamonkey.hivtrace.betweenness_centrality('Beverly', trace_results);
+
+          heather.should.be.approximately(.389, delta);
+          fernando.should.be.approximately(.231, delta);
+          garth.should.be.approximately(.231, delta);
+          ike.should.be.approximately(.222, delta);
+          diane.should.be.approximately(.102, delta);
+          andre.should.be.approximately(.023, delta);
+          beverly.should.be.approximately(.023, delta);
+
+          done();
+
+      }
+
+    });
+
+  });
+
+  it('should finish in due time', function(done) {
+
     done();
 
   });
 
+  it('check adjacency list', function(done) {
+
+    jsdom.env({
+      url: "http://datamonkey.org",
+      src: [d3_js, underscore, hiv_utils],
+      done: function (err, window) {
+
+          var datamonkey = window.datamonkey;
+
+          var answers = { 
+           "Andre"    : [ "Beverly","Carol","Diane","Fernando" ],
+           "Beverly"  : [ "Andre", "Diane", "Ed","Garth" ],
+           "Carol"    : [ "Andre", "Diane", "Fernando" ],
+           "Diane"    : [ "Andre", "Beverly", "Carol", "Ed", "Fernando", "Garth" ],
+           "Ed"       : [ "Beverly", "Diane", "Garth" ],
+           "Fernando" : [ "Andre", "Carol", "Diane", "Garth", "Heather" ],
+           "Garth"    : [ "Beverly", "Diane", "Ed", "Fernando", "Heather"],
+           "Heather"  : [ "Fernando", "Garth", "Ike"],
+           "Ike"      : [ "Heather", "Jane" ],
+           "Jane"     : [ "Ike" ] 
+          };
+
+          var adj_list = datamonkey.hivtrace.cluster_adjacency_list(trace_results);
+
+          for (id in adj_list) {
+            ids = adj_list[id].map(function(n) { return n.id } );
+            _.difference(ids, answers[id]).length.should.be.equal(0);
+          }
+
+          done();
+      }
+    });
+  });
 });
