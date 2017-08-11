05f41e04db64c174cf63d90fda91316d34908044
Steven Weaver <sweaver@temple.edu>
2017-08-11 11:50:48 -0400

    adding new analysis page summaries

--- src/entry.js
+++ src/entry.js
@@ -7,7 +7,12 @@ window._ = _;
 require("font-awesome/css/font-awesome.css");
 require('./less/bootstrap.less');
 
-require('bootstrap');
+var datamonkey = require('./js/datamonkey.js');
+datamonkey.helpers = require('./js/datamonkey_helpers.js');
 
-var gard_form = require('jsx/gard_form.jsx');
+var gard_form = require('jsx/forms/gard.jsx');
+var slac_form = require('jsx/forms/slac.jsx');
+
 window.gard_form = gard_form;
+window.slac_form = slac_form;
+window.datamonkey = datamonkey;
