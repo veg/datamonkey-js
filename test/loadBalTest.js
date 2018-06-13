74e3b4a74b682bc7b856fc930f773552bd363ef1
Kevin Levitz <kevin@klevitz.com>
2018-06-13 16:45:35 -0400

    Update loadBalTest.js

--- test/loadBalTest.js
+++ test/loadBalTest.js
@@ -37,3 +37,15 @@ describe('loadBalance function: test iteration with string cross check', functio
           });
   });
 });
+
+describe('loadBalance function: single string cross check', function() {
+  var history = [];
+  //var urls = ["www.1.com", "www.2.com", "www.3.com", "www.4.com"]; //replace this with URL array
+  it('Returns one string that is cross referenced with URLs array', function(){
+    loadBalance(urls, (err, return_url) => {
+      history.push(return_url);
+      assert.equal((_.intersection(history, urls)).length, 1);
+    })
+
+  });
+});
