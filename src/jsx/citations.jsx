86011511937c66fbfba47e21ce9ffd3a410e8c09
Michael Li <tud24970@temple.edu>
2018-09-05 17:03:01 -0400

    WIP: citations page

--- src/jsx/citations.jsx
+++ src/jsx/citations.jsx
@@ -17,16 +17,16 @@ class Citations extends React.Component {
             application notes.
           </p>
           <br />
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <h3 className="panel-title">Datamonkey</h3>
+          <div className="dm-panel dm-panel-default panel panel-default">
+            <div className="dm-panel-heading panel-heading">
+              <h3 className="dm-panel-title panel-title">Datamonkey</h3>
             </div>
-            <div className="panel-body">
-              <h4>
+            <div className="dm-panel-body panel-body">
+              <div>
                 Steven Weaver, Stephen D. Shank, Stephanie J. Spielman, Michael
                 Li, Spencer V. Muse, Sergei L. Kosakovsky Pond
-              </h4>
-              <h5>
+              </div>
+              <div>
                 <a
                   target="_blank"
                   href="https://academic.oup.com/mbe/article/35/3/773/4782511"
@@ -38,15 +38,16 @@ class Citations extends React.Component {
                   Datamonkey 2.0: a modern web application for characterizing
                   selective and other evolutionary processes
                 </strong>
-              </h5>
-              <p>Mol. Biol. Evol. 35(3):773–777</p>
+                <p>Mol. Biol. Evol. 35(3):773–777</p>
+              </div>
+
               <hr />
 
-              <h4>
+              <div>
                 Wayne Delport, Art F. Poon, Simon D. W. Frost and Sergei L.
                 Kosakovsky Pond
-              </h4>
-              <h5>
+              </div>
+              <div>
                 <a
                   target="_blank"
                   href="https://academic.oup.com/bioinformatics/article-lookup/doi/10.1093/bioinformatics/btq429"
@@ -58,11 +59,13 @@ class Citations extends React.Component {
                   Datamonkey 2010: a suite of phylogenetic analysis tools for
                   evolutionary biology
                 </strong>
-              </h5>
-              <p>Bioinformatics 26(19): 2455-2457</p>
+                <p>Bioinformatics 26(19): 2455-2457</p>
+              </div>
+
               <hr />
-              <h4>Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)</h4>
-              <h5>
+
+              <div>Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)</div>
+              <div>
                 <a
                   target="_blank"
                   href="http://bioinformatics.oxfordjournals.org/cgi/content/abstract/21/10/2531?ijkey=a5yRfKOnDZ1Kb6J&keytype=ref"
@@ -74,8 +77,8 @@ class Citations extends React.Component {
                   Datamonkey: rapid detection of selective pressure on
                   individual sites of codon alignments
                 </strong>
-              </h5>
-              <p>Bioinformatics 21(10): 2531-2533</p>
+                <p>Bioinformatics 21(10): 2531-2533</p>
+              </div>
             </div>
           </div>
         </div>
@@ -108,16 +111,16 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">HyPhy</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">HyPhy</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   Sergei L. Kosakovsky Pond, Simon D. W. Frost and Spencer V.
                   Muse (2005)
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://bioinformatics.oxfordjournals.org/cgi/content/short/21/5/676"
@@ -129,8 +132,8 @@ class Citations extends React.Component {
                     />
                   </a>
                   <strong>HyPhy: hypothesis testing using phylogenies</strong>
-                </h5>
-                <p>Bioinformatics 21(5): 676-679</p>
+                  <p>Bioinformatics 21(5): 676-679</p>
+                </div>
               </div>
             </div>
           </div>
@@ -139,16 +142,16 @@ class Citations extends React.Component {
         <div className="row">
           <div className="col">
             <h2>Method Citations</h2>
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">aBSREL</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">aBSREL</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   M. D. Smith, J. O. Wertheim, S. Weaver, B. Murrell, K.
                   Scheffler and S. L. Kosakovsky Pond
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://dx.doi.org/10.1093/molbev/msv022"
@@ -162,14 +165,16 @@ class Citations extends React.Component {
                     Less Is More: An Adaptive Branch-Site Random Effects Model
                     for Efficient Detection of Episodic Diversifying Selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 32: 1342-1353</p>
+                  <p>Molecular Biology and Evolution 32: 1342-1353</p>
+                </div>
+
                 <hr />
-                <h4>
+
+                <div>
                   Sergei L. Kosakovsky Pond, Ben Murrell, Mathieu Fourment,
                   Simon D. W. Frost, Wayne Delport and Konrad Scheffler (2011)
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://mbe.oxfordjournals.org/content/early/2011/06/11/molbev.msr125.abstract"
@@ -184,8 +189,8 @@ class Citations extends React.Component {
                     A random effects branch-site model for detecting episodic
                     diversifying selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 28(11): 3033-3043</p>
+                  <p>Molecular Biology and Evolution 28(11): 3033-3043</p>
+                </div>
               </div>
             </div>
           </div>
@@ -193,17 +198,17 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">BUSTED</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">BUSTED</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   B. Murrell, S. Weaver, M. D. Smith, J. O. Wertheim, S.
                   Murrell, A. Aylward and K. Eren, T. Pollner, D. P. Martin, D.
                   M. Smith, K. Scheffler and S. L. Kosakovsky Pond
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="https://academic.oup.com/mbe/article-lookup/doi/10.1093/molbev/msv035"
@@ -216,8 +221,8 @@ class Citations extends React.Component {
                   <strong>
                     Gene-Wide Identification of Episodic Selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 32(5) 1365-1371</p>
+                  <p>Molecular Biology and Evolution 32(5) 1365-1371</p>
+                </div>
               </div>
             </div>
           </div>
@@ -225,13 +230,15 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">FEL</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">FEL</h3>
               </div>
-              <div className="panel-body">
-                <h4>Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)</h4>
-                <h5>
+              <div className="dm-panel-body panel-body">
+                <div>
+                  Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://mbe.oxfordjournals.org/cgi/content/short/22/5/1208"
@@ -245,8 +252,8 @@ class Citations extends React.Component {
                     Not So Different After All: A Comparison of Methods for
                     Detecting Amino Acid Sites Under Selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 22(5): 1208-1222</p>
+                  <p>Molecular Biology and Evolution 22(5): 1208-1222</p>
+                </div>
               </div>
             </div>
           </div>
@@ -254,16 +261,16 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">FUBAR</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">FUBAR</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   B. Murrell, S. Moola, A. Mabona, T. Weighill, D. Sheward, S.
                   L. Kosakovsky Pond and K. Scheffler
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="https://academic.oup.com/mbe/article/30/5/1196/998247/FUBAR-A-Fast-Unconstrained-Bayesian-AppRoximation"
@@ -277,8 +284,8 @@ class Citations extends React.Component {
                     FUBAR: a fast, unconstrained bayesian approximation for
                     inferring selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 30(5) : 1196-1205</p>
+                  <p>Molecular Biology and Evolution 30(5) : 1196-1205</p>
+                </div>
               </div>
             </div>
           </div>
@@ -286,16 +293,16 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">GARD</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">GARD</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   Sergei L Kosakovsky Pond, David Posada, Michael B Gravenor,
                   Christopher H Woelk and Simon DW Frost
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://mbe.oxfordjournals.org/cgi/content/full/23/10/1891"
@@ -309,8 +316,8 @@ class Citations extends React.Component {
                     Automated Phylogenetic Detection of Recombination Using a
                     Genetic Algorithm
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 23(10): 1891-1901</p>
+                  <p>Molecular Biology and Evolution 23(10): 1891-1901</p>
+                </div>
               </div>
             </div>
           </div>
@@ -318,16 +325,16 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">MEME</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">MEME</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   Ben Murrell, Joel O. Wertheim, Sasha Moola, Thomas Weighill,
                   Konrad Scheffler and Sergei L. Kosakovsky Pond (2012)
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://www.plosgenetics.org/article/info%3Adoi%2F10.1371%2Fjournal.pgen.1002764"
@@ -341,8 +348,8 @@ class Citations extends React.Component {
                     Detecting Individual Sites Subject to Episodic Diversifying
                     Selection
                   </strong>
-                </h5>
-                <p>PLoS Genetics 8(7): e1002764</p>
+                  <p>PLoS Genetics 8(7): e1002764</p>
+                </div>
               </div>
             </div>
           </div>
@@ -350,16 +357,16 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">RELAX</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">RELAX</h3>
               </div>
-              <div className="panel-body">
-                <h4>
+              <div className="dm-panel-body panel-body">
+                <div>
                   J. O. Wertheim, B. Murrell, M. D. Smith, S. L. Kosakovsky Pond
                   and K. Scheffler
-                </h4>
-                <h5>
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="https://academic.oup.com/mbe/article-lookup/doi/10.1093/molbev/msu400"
@@ -373,8 +380,8 @@ class Citations extends React.Component {
                     RELAX: detecting relaxed selection in a phylogenetic
                     framework
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 32(3): 820-832</p>
+                  <p>Molecular Biology and Evolution 32(3): 820-832</p>
+                </div>
               </div>
             </div>
           </div>
@@ -382,13 +389,15 @@ class Citations extends React.Component {
 
         <div className="row">
           <div className="col">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">SLAC</h3>
+            <div className="dm-panel dm-panel-default panel panel-default">
+              <div className="dm-panel-heading panel-heading">
+                <h3 className="dm-panel-title panel-title">SLAC</h3>
               </div>
-              <div className="panel-body">
-                <h4>Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)</h4>
-                <h5>
+              <div className="dm-panel-body panel-body">
+                <div>
+                  Sergei L. Kosakovsky Pond and Simon D. W. Frost (2005)
+                </div>
+                <div>
                   <a
                     target="_blank"
                     href="http://mbe.oxfordjournals.org/cgi/content/short/22/5/1208"
@@ -402,8 +411,8 @@ class Citations extends React.Component {
                     Not So Different After All: A Comparison of Methods for
                     Detecting Amino Acid Sites Under Selection
                   </strong>
-                </h5>
-                <p>Molecular Biology and Evolution 22(5): 1208-1222</p>
+                  <p>Molecular Biology and Evolution 22(5): 1208-1222</p>
+                </div>
               </div>
             </div>
           </div>
