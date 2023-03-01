9d6b868fff48ac0eafc670e8df43db168032d05d
Steven Weaver <sweaver@temple.edu>
2023-03-01 13:44:10 -0500

    initial nrm commit

---
+++ app/models/nrm.js
@@ -0,0 +1,128 @@
+var mongoose = require("mongoose"),
+  path = require("path"),
+  logger = require("../../lib/logger"),
+  helpers = require(__dirname + "/../../lib/helpers.js");
+
+require("mongoose-schema-extend");
+
+var AnalysisSchema = require(__dirname + "/analysis");
+
+var NRM = AnalysisSchema.extend({
+  last_status_msg: String,
+  results: Object
+});
+
+NRM.virtual("analysistype").get(function () {
+  return "nrm";
+});
+
+NRM.virtual("pmid").get(function () {
+  return "TBD";
+});
+
+NRM.virtual("max_sequences").get(function () {
+  return 1000;
+});
+
+/**
+ * Filename of document's file upload
+ */
+NRM.virtual("status_stack").get(function () {
+  return ["queue", "running", "completed"];
+});
+
+NRM.virtual("upload_redirect_path").get(function () {
+  return path.join("/nrm/", String(this._id));
+});
+
+/**
+ * Complete file path for document's file upload
+ */
+NRM.virtual("filepath").get(function () {
+  return path.resolve(__dirname + "/../../uploads/msa/" + this._id + ".fasta");
+});
+
+/**
+ * URL for a nrm path
+ */
+NRM.virtual("url").get(function () {
+  return "http://" + setup.host + "/nrm/" + this._id;
+});
+
+/**
+ * Shared API / Web request job spawn
+ */
+NRM.statics.spawn = function (fn, options, callback) {
+  const Msa = mongoose.model("Msa");
+
+  var nrm = new this();
+
+  let gencodeid = options.gencodeid,
+    datatype = options.datatype;
+
+  nrm.datatype = options.datatype;
+  nrm.gencodeid = options.gencodeid;
+  nrm.mail = options.mail;
+  nrm.rate_classes = options.rate_classes;
+  nrm.triple_islands = options.triple_islands;
+
+  const connect_callback = function (data) {
+    if (data == "connected") {
+      logger.log("connected");
+    }
+  };
+
+  Msa.parseFile(fn, datatype, gencodeid, (err, msa) => {
+    if (err) {
+      logger.error(err);
+      callback(err);
+      return;
+    }
+    // Check if msa exceeds limitations
+    if (msa.sites > nrm.max_sites) {
+      const error =
+        "Site limit exceeded! Sites must be less than " + nrm.max_sites;
+      logger.error(error);
+      callback(error);
+      return;
+    }
+
+    if (msa.sequences > nrm.max_sequences) {
+      var error =
+        "Sequence limit exceeded! Sequences must be less than " +
+        nrm.max_sequences;
+      logger.error(error);
+      callback(error);
+      return;
+    }
+
+    nrm.msa = msa;
+    nrm.status = nrm.status_stack[0];
+
+    nrm.save((err, nrm_result) => {
+      if (err) {
+        logger.error("nrm save failed");
+        callback(err);
+        return;
+      }
+
+      function move_cb(err, result) {
+        if (err) {
+          logger.error(
+            "nrm rename failed" +
+              " Errored on line 113~ within models/nrm.js :: move_cb " +
+              err
+          );
+          callback(err, null);
+        } else {
+          var to_send = nrm;
+          to_send.upload_redirect_path = nrm.upload_redirect_path;
+          this.submitJob(nrm_result, connect_callback);
+          callback(null, nrm);
+        }
+      }
+      helpers.moveSafely(fn, nrm_result.filepath, move_cb.bind(this));
+    });
+  });
+};
+module.exports = mongoose.model("NRM", NRM);
---
+++ src/jsx/forms/nrm.jsx
@@ -0,0 +1,192 @@
+var React = require("react"),
+  ReactDOM = require("react-dom"),
+  createReactClass = require("create-react-class");
+
+var NRMForm = createReactClass({
+
+  onMailChange: function () {},
+
+  submit: function (e) {
+
+    e.preventDefault();
+
+    $("#file-progress").removeClass("hidden");
+
+    var formData = new FormData();
+    var file = document.getElementById("seq-file").files[0];
+
+    formData.append("files", file);
+    formData.append("gencodeid", $("select[name='gencodeid']").val());
+
+    formData.append("triple_islands", $("select[name='triple_islands']").val());
+
+    formData.append("rate_classes", $("input[name='rate_classes']").val());
+
+    formData.append("receive_mail", $("input[name='mail']").val().length > 0);
+    formData.append("mail", $("input[name='mail']").val());
+
+    var action_url = $("#msa-form").attr("action");
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("post", action_url, true);
+
+    xhr.upload.onprogress = function (e) {
+      if (e.lengthComputable) {
+        var percentage = (e.loaded / e.total) * 100;
+        $("#file-progress").css("display", "block");
+        $("#seq-file").css("display", "none");
+        $(".progress .progress-bar").css("width", percentage + "%");
+      }
+    };
+
+    xhr.onerror = function (e) {
+      $("#file-progress").html(e);
+    };
+
+    xhr.onload = function (res) {
+      // Replace field with green text, name of file
+      var result = JSON.parse(this.responseText);
+
+      if (_.has(result, "error")) {
+        $("#modal-error-msg").text(result.error);
+        $("#errorModal").modal();
+        $("#file-progress").css("display", "none");
+        $("#seq-file").css("display", "block");
+        $(".progress .progress-bar").css("width", "0%");
+      } else if ("error" in result.analysis) {
+        $("#modal-error-msg").text(result.error);
+        $("#errorModal").modal();
+        $("#file-progress").css("display", "none");
+        $("#seq-file").css("display", "block");
+        $(".progress .progress-bar").css("width", "0%");
+      } else if ("upload_redirect_path" in result) {
+        window.location.href = result.upload_redirect_path;
+      } else {
+        $("#modal-error-msg").text(
+          "We received data in an unexpected format from the server."
+        );
+        $("#errorModal").modal();
+        $("#file-progress").css("display", "none");
+        $("#seq-file").css("display", "block");
+        $(".progress .progress-bar").css("width", "0%");
+      }
+    };
+
+    xhr.send(formData);
+  },
+
+  render: function () {
+    return (
+      <form
+        id="msa-form"
+        className="form-horizontal upload-form"
+        name="uploadform"
+        enctype="multipart/form-data"
+        method="post"
+        action={this.props.post_to}
+      >
+        <div id="seq-file-div" className="upload-div">
+          <input id="seq-file" type="file" name="files" />
+          <div
+            id="file-progress"
+            className="progress progress-striped active hidden"
+          >
+            <div
+              className="progress-bar"
+              role="progressbar"
+              aria-valuenow="0"
+              aria-valuemin="1"
+              aria-valuemax="100"
+            >
+              <span className="sr-only">0% Complete</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="upload-div">
+          <label id="geneticcode-content">
+            Genetic Code
+            <a href="/help#genetic-code" target="_blank">
+              <sup>?</sup>
+            </a>
+          </label>
+          <select name="gencodeid">
+            <option value="0">Universal code</option>
+
+            <option value="1">Vertebrate mitochondrial DNA code</option>
+
+            <option value="2">Yeast mitochondrial DNA code</option>
+
+            <option value="3">
+              Mold, Protozoan and Coelenterate mt; Mycloplasma/Spiroplasma
+            </option>
+
+            <option value="4">Invertebrate mitochondrial DNA code</option>
+
+            <option value="5">
+              Ciliate, Dasycladacean and Hexamita Nuclear code
+            </option>
+
+            <option value="6">Echinoderm mitochondrial DNA code</option>
+
+            <option value="7">Euplotid Nuclear code</option>
+
+            <option value="8">Alternative Yeast Nuclear code</option>
+
+            <option value="9">Ascidian mitochondrial DNA code</option>
+
+            <option value="10">Flatworm mitochondrial DNA code</option>
+
+            <option value="11">Blepharisma Nuclear code</option>
+          </select>
+        </div>
+
+        <div className="upload-div">
+          <label
+            id="datatype-content"
+            data-toggle="tooltip"
+            data-placement="top"
+            title="Use a separate rate parameter for synonymous triple-hit substitutions"
+          >
+            Triple Islands
+          </label>
+          <select name="triple_islands">
+            <option value="No">No</option>
+            <option value="Yes">Yes</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label id="datatype-content">Notify When Completed?</label>
+          <div>
+            <input
+              name="mail"
+              type="text"
+              className="form-control"
+              placeholder="Email Address"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          id="submit-button"
+          className="run-analysis-button-text dm-continue-btn btn float-right"
+          onClick={this.submit}
+        >
+          Run Analysis <span className="fa fa-play" />
+        </button>
+
+        <div style={{ paddingBottom: "30px" }} />
+      </form>
+    );
+  },
+});
+
+function render_nrm_form() {
+  ReactDOM.render(
+    <NRMForm post_to={"/nrm"} />,
+    document.getElementById("upload-form")
+  );
+}
+
+module.exports = render_nrm_form;
